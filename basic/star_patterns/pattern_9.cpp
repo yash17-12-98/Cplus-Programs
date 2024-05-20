8061d649851b4584393cde51b8142e70f2aeaec0
yash <yash.suthar@techalchemy.com>
2024-05-20 18:14:36 +0530

    Written program for patten 9

---
+++ basic/star_patterns/pattern_9.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+
+
+int main(){
+    int n;
+
+    cout << "Enter the value of n: ";
+
+    cin >> n;
+
+    int i = 1;
+
+    while(i <= n){
+
+        int j = 1; 
+        while(j <= i){
+            cout << i-j+1 << " ";
+            j++;
+        }
+
+        cout << endl;
+        i++;
+    }
+}
