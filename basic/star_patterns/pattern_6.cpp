4c18f2f9644f0a3a6206837dcf3a2501e04ce36f
yash <yash.suthar@techalchemy.com>
2024-05-20 17:43:28 +0530

    Written code for pattern 6 program

---
+++ basic/star_patterns/pattern_6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+// 1 
+// 2 2
+// 3 3 3
+// 4 4 4 4 
+
+int main(){
+    int n;
+
+    cout << "Enter the value of n: ";
+
+    cin >> n;
+
+    int i = 1;
+
+    while(i <= n){
+        int j = 1;
+        while(j <= i){
+            cout << i << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+}
