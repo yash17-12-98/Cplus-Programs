397d488e66dceaf8ba8b770d71595c9ae75e7fa9
yash <yash.suthar@techalchemy.com>
2024-05-21 11:09:26 +0530

    Written code for pattern 17 program

---
+++ basic/star_patterns/pattern_17.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+
+    cout << "Enter the value of n:";
+
+    cin >> n;
+
+    int i = 1;
+
+    while(i <= n){
+        int j = n;
+
+        while(j >= i){
+            j--;
+            cout << "* ";
+        }
+        cout << endl;
+        i++;
+    
+    }
+}
