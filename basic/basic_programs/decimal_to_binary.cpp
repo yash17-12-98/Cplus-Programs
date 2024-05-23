367c361c9cdf88d42964a259e8ae10ecb1bbbfd5
yash <yash.suthar@techalchemy.com>
2024-05-23 14:44:34 +0530

    Written code for binary to decimal

--- basic/basic_programs/decimal_to_binary.cpp
+++ basic/basic_programs/decimal_to_binary.cpp
@@ -4,6 +4,9 @@ using namespace std;
 // Input: n = 12
 // Output: 1100 
 
+// Input: n = 0
+// Output: 0
+
 int main(){
     int n;
 
@@ -18,7 +21,9 @@ int main(){
     while(n != 0){
         int bit = n & 1;
 
+     
         ans = (pow(10, i) * bit) + ans;
+    
 
         n = n >> 1;
 
