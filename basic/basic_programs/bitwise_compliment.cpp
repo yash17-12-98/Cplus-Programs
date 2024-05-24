48fbc147da6027918227e35b6bbe5feee6d33644
yash <yash.suthar@techalchemy.com>
2024-05-24 11:34:12 +0530

    Written code with another approach for bitwise compliment base 10 integer

--- basic/basic_programs/bitwise_compliment.cpp
+++ basic/basic_programs/bitwise_compliment.cpp
@@ -9,22 +9,44 @@ using namespace std;
 // Output: 0
 // Explanation: 7 is "111" in binary, with complement "000" in binary, which is 0 in base-10.
 
+// int main(){
+//     int n;
+//     cout << "Enter the value of n: ";
+
+//     cin >> n;
+
+//     int m = n;
+
+//     int mask = 0;
+
+//     while(m != 0){
+//         mask = (mask << 1) | 1;
+//         m = m >> 1;
+//     }
+
+//     int ans = (~n) & mask;
+
+//     cout << "Answer: " << ans << endl;
+// }
+
 int main(){
     int n;
+
     cout << "Enter the value of n: ";
 
     cin >> n;
 
-    int m = n;
-
-    int mask = 0;
-
-    while(m != 0){
-        mask = (mask << 1) | 1;
-        m = m >> 1;
+    if(n == 0){
+          cout << "Answer: " << 1 << endl;
+          return 1;
     }
 
-    int ans = (~n) & mask;
+    int x = 1;
 
-    cout << "Answer: " << ans << endl;
+    while(x <= n){
+        n = n ^ x;
+        x = x << 1;
+    }
+
+    cout << "Answer: " << n << endl;
 }
--- basic/basic_programs/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-
-// Output: 0
-// Explanation: 7 is "111" in bin
